73b25357aa094639b5343c92e089ef7a13a4eec9
LRYP <2213469156@qq.com>
2025-07-01 23:37:25 +0800

    自动提交：更新 QueryCoursesByCourseGroupMessagePlanner (APIPlan) 的代码

--- src/main/scala/Impl/QueryCoursesByCourseGroupMessagePlanner.scala
+++ src/main/scala/Impl/QueryCoursesByCourseGroupMessagePlanner.scala
@@ -1,130 +1,116 @@
 package Impl
 
 
-/**
- * Planner for QueryCoursesByCourseGroup:
- * 查询课程组特定课程信息
- */
+import Objects.CourseManagementService.{CourseTime, CourseGroup, DayOfWeek, TimePeriod, CourseInfo}
+import Utils.CourseManagementProcess.fetchCourseGroupByID
 import Common.API.{PlanContext, Planner}
 import Common.DBAPI._
 import Common.Object.SqlParameter
 import Common.ServiceUtils.schemaName
-import Utils.CourseManagementProcess.fetchCourseGroupByID
-import Objects.CourseManagementService.CourseInfo
+import cats.effect.IO
+import org.slf4j.LoggerFactory
+import io.circe._
+import io.circe.syntax._
+import io.circe.generic.auto._
+import org.joda.time.DateTime
+import cats.implicits.*
+import io.circe._
+import io.circe.syntax._
+import io.circe.generic.auto._
+import org.joda.time.DateTime
+import cats.implicits.*
+import Common.DBAPI._
+import Common.API.{PlanContext, Planner}
+import cats.effect.IO
+import Common.Object.SqlParameter
+import Common.Serialize.CustomColumnTypes.{decodeDateTime,encodeDateTime}
+import Common.ServiceUtils.schemaName
 import Objects.CourseManagementService.CourseTime
+import Objects.CourseManagementService.CourseGroup
 import Objects.CourseManagementService.DayOfWeek
 import Objects.CourseManagementService.TimePeriod
-import Objects.CourseManagementService.CourseGroup
-import org.slf4j.LoggerFactory
-import cats.effect.IO
-import io.circe.Json
-import io.circe.parser.decode
-import io.circe.syntax._
-import io.circe.generic.auto._
-import org.joda.time.DateTime
-import cats.implicits.*
-import Common.Serialize.CustomColumnTypes.{decodeDateTime, encodeDateTime}
-import io.circe._
-import io.circe.syntax._
-import io.circe.generic.auto._
-import org.joda.time.DateTime
-import cats.implicits.*
-import Common.DBAPI._
-import Common.API.{PlanContext, Planner}
-import cats.effect.IO
-import Common.Object.SqlParameter
-import Common.Serialize.CustomColumnTypes.{decodeDateTime,encodeDateTime}
-import Common.ServiceUtils.schemaName
 import Objects.CourseManagementService.CourseInfo
 import cats.implicits._
-import io.circe._
 import Common.Serialize.CustomColumnTypes.{decodeDateTime,encodeDateTime}
+import Objects.CourseManagementService.CourseInfo
 
 case class QueryCoursesByCourseGroupMessagePlanner(
-                                                    courseGroupID: Int,
-                                                    override val planContext: PlanContext
-                                                  ) extends Planner[List[CourseInfo]] {
-
+  courseGroupID: Int,
+  override val planContext: PlanContext
+) extends Planner[List[CourseInfo]] {
   val logger = LoggerFactory.getLogger(this.getClass.getSimpleName + "_" + planContext.traceID.id)
 
-  override def plan(using planContext: PlanContext): IO[List[CourseInfo]] = {
+  override def plan(using PlanContext): IO[List[CourseInfo]] = {
     for {
-      // Step 1: 验证 CourseGroupID 的合法性
-      _ <- IO(logger.info(s"[Step 1] 验证 CourseGroupID=${courseGroupID} 的合法性"))
-      courseGroup <- validateCourseGroupID(courseGroupID)
-      _ <- IO(logger.info(s"[验证完成] CourseGroupID合法: ${courseGroup}"))
+      // Step 1: Validate `courseGroupID` parameter.
+      _ <- IO(logger.info(s"验证courseGroupID=${courseGroupID}的有效性"))
+      courseGroupOpt <- validateCourseGroupID(courseGroupID)
+      _ <- IO(logger.info(s"验证完成，课程组: ${courseGroupOpt.getOrElse("不存在")}"))
 
-      // Step 2: 查询课程数据
-      _ <- IO(logger.info(s"[Step 2] 查询课程数据，CourseGroupID=${courseGroupID}"))
+      // Step 2: Query course data.
+      _ <- IO(logger.info(s"开始查询与课程组${courseGroupID}相关的课程数据"))
       courses <- queryCourses(courseGroupID)
-      _ <- IO(logger.info(s"[查询完成] 返回课程信息封装，数量=${courses.size}"))
+      _ <- IO(logger.info(s"查询课程数据完成，共查询到${courses.size}条课程记录"))
 
     } yield courses
   }
 
-  private def validateCourseGroupID(courseGroupID: Int)(using PlanContext): IO[CourseGroup] = {
-    logger.info(s"调用 fetchCourseGroupByID 方法检查课程组是否存在, CourseGroupID=${courseGroupID}")
+  /**
+   * Validate the courseGroupID by fetching related course group data.
+   * If the course group does not exist, throw an error.
+   */
+  private def validateCourseGroupID(courseGroupID: Int)(using PlanContext): IO[Option[CourseGroup]] = {
     fetchCourseGroupByID(courseGroupID).flatMap {
       case None =>
-        val errorMessage = s"课程组ID ${courseGroupID} 不存在，无法查询课程信息"
-        IO(logger.error(errorMessage)) >>
-          IO.raiseError(new IllegalArgumentException(errorMessage))
+        IO {
+          val errorMessage = s"课程组[courseGroupID=${courseGroupID}]不存在，请确认输入参数是否正确"
+          logger.error(errorMessage)
+          throw new IllegalArgumentException(errorMessage)
+        }
       case Some(courseGroup) =>
-        IO(logger.info(s"课程组验证通过，可以访问: ${courseGroup}")).map(_ => courseGroup)
+        IO.pure(Some(courseGroup))
     }
   }
 
+  /**
+   * Query all courses associated with the courseGroupID and map them to a list of CourseInfo instances.
+   */
   private def queryCourses(courseGroupID: Int)(using PlanContext): IO[List[CourseInfo]] = {
     val sql =
       s"""
-       SELECT course_id, course_capacity, time, location, teacher_id
-       FROM ${schemaName}.course_table
-       WHERE course_group_id = ?;
+SELECT course_id, course_group_id, course_capacity, time, location, teacher_id
+FROM ${schemaName}.course_table
+WHERE course_group_id = ?
        """.stripMargin
 
-    for {
-      _ <- IO(logger.info(s"[QueryCourses] 执行查询SQL：${sql}"))
-      rows <- readDBRows(sql, List(SqlParameter("Int", courseGroupID.toString)))
-      _ <- IO(logger.info(s"[QueryCourses] 数据库查询完成，共返回 ${rows.size} 行，准备封装"))
+    logger.info(s"执行SQL: $sql [参数: courseGroupID=$courseGroupID]")
 
-      // Traverse 封装课程信息
-      courses <- rows.traverse { row =>
-        IO {
+    readDBRows(sql, List(SqlParameter("Int", courseGroupID.toString))).flatMap { rows =>
+      IO {
+        rows.map { row =>
           val courseID = decodeField[Int](row, "course_id")
           val courseCapacity = decodeField[Option[Int]](row, "course_capacity").getOrElse(0)
-          val timeJsonString = decodeField[String](row, "time")
-          val location = decodeField[String](row, "location")
+          val timeJson = decodeField[String](row, "time")
+          val courseTime = decodeType[List[CourseTime]](timeJson)
+          val location = decodeField[Option[String]](row, "location").getOrElse("")
           val teacherID = decodeField[Int](row, "teacher_id")
+          val preselectedStudentsSize = 0 // 默认值：提前选课的学生数量不支持动态读取
+          val selectedStudentsSize = 0   // 默认值：最终已选课程的学生数量
+          val waitingListSize = 0        // 默认值：等待名单上的学生数量
 
-          // 封装时间信息
-          val time = decode[List[Json]](timeJsonString).fold(
-            ex => {
-              logger.error(s"[课程时间解析失败] 错误信息：${ex.getMessage}, 原始时间JSON：${timeJsonString}")
-              throw ex
-            },
-            _.map { timeJson =>
-              val dayOfWeek = DayOfWeek.fromString(decodeField[String](timeJson, "dayOfWeek"))
-              val timePeriod = TimePeriod.fromString(decodeField[String](timeJson, "timePeriod"))
-              CourseTime(dayOfWeek, timePeriod)
-            }
-          )
-
-          // 封装 CourseInfo 对象
           CourseInfo(
             courseID = courseID,
             courseCapacity = courseCapacity,
-            time = time,
+            time = courseTime,
             location = location,
             courseGroupID = courseGroupID,
             teacherID = teacherID,
-            preselectedStudentsSize = 0, // 不涉及预选
-            selectedStudentsSize = 0, // 不涉及最终选择
-            waitingListSize = 0 // 不涉及等待名单
+            preselectedStudentsSize = preselectedStudentsSize,
+            selectedStudentsSize = selectedStudentsSize,
+            waitingListSize = waitingListSize
           )
         }
       }
-
-      _ <- IO(logger.info(s"[封装完成] CourseInfo 已完成转换，数量=${courses.size}"))
-    } yield courses
+    }
   }
 }
