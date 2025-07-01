8c462484392533204d0cea04d39f0945e9f0f565
LRYP <2213469156@qq.com>
2025-07-01 23:35:19 +0800

    自动提交：更新 QueryOwnCourseGroupsMessagePlanner (APIPlan) 的代码

--- src/main/scala/Impl/QueryOwnCourseGroupsMessagePlanner.scala
+++ src/main/scala/Impl/QueryOwnCourseGroupsMessagePlanner.scala
@@ -1,25 +1,19 @@
 package Impl
 
 
-/**
- * Planner for QueryOwnCourseGroupsMessage: 用于处理查看课程组的功能需求
- */
-import APIs.UserAuthService.VerifyTokenValidityMessage
-import Objects.CourseManagementService.CourseGroup
-import Objects.UserAccountService.UserRole
-import Objects.UserAccountService.SafeUserInfo
-import Utils.CourseManagementProcess.validateTeacherToken
 import Common.API.{PlanContext, Planner}
 import Common.DBAPI._
 import Common.Object.SqlParameter
 import Common.ServiceUtils.schemaName
+import Objects.CourseManagementService.CourseGroup
+import Utils.CourseManagementProcess.validateTeacherToken
 import cats.effect.IO
 import org.slf4j.LoggerFactory
-import org.joda.time.DateTime
 import io.circe._
-import io.circe.generic.auto._
 import io.circe.syntax._
-import cats.implicits._
+import io.circe.generic.auto._
+import org.joda.time.DateTime
+import cats.implicits.*
 import Common.Serialize.CustomColumnTypes.{decodeDateTime, encodeDateTime}
 import io.circe._
 import io.circe.syntax._
@@ -32,75 +26,67 @@ import cats.effect.IO
 import Common.Object.SqlParameter
 import Common.Serialize.CustomColumnTypes.{decodeDateTime,encodeDateTime}
 import Common.ServiceUtils.schemaName
+import APIs.UserAuthService.VerifyTokenValidityMessage
+import Objects.UserAccountService.UserRole
+import Objects.UserAccountService.SafeUserInfo
 import Utils.CourseManagementProcess.validateTeacherToken
-import cats.implicits.*
 import Common.Serialize.CustomColumnTypes.{decodeDateTime,encodeDateTime}
 
 case class QueryOwnCourseGroupsMessagePlanner(
                                                teacherToken: String,
                                                override val planContext: PlanContext
                                              ) extends Planner[List[CourseGroup]] {
-  private val logger = LoggerFactory.getLogger(this.getClass.getSimpleName + "_" + planContext.traceID.id)
 
-  /**
-   * Plan Method (核心实现逻辑)
-   */
-  override def plan(using planContext: PlanContext): IO[List[CourseGroup]] = {
+  val logger = LoggerFactory.getLogger(this.getClass.getSimpleName + "_" + planContext.traceID.id)
+
+  override def plan(using PlanContext): IO[List[CourseGroup]] = {
     for {
-      // Step 1: 验证教师 token 并获取教师 ID
-      teacherID <- validateTokenAndFetchTeacherID(teacherToken)
+      // Step 1: Validate the teacher token and retrieve teacherID
+      _ <- IO(logger.info(s"[Step 1] 验证教师 token: ${teacherToken}"))
+      maybeTeacherID <- validateTeacherToken(teacherToken)
 
-      // Step 2: 查询教师 ID 创建的课程组信息
-      _ <- IO(logger.info(s"开始查询课程组信息，教师ID为: ${teacherID}"))
-      courseGroups <- queryCourseGroupsByTeacherID(teacherID)
+      teacherID <- maybeTeacherID match {
+        case None =>
+          IO(logger.error("[Step 1.1] 教师鉴权失败，无法继续操作，返回错误")) >>
+            IO.raiseError(new IllegalArgumentException("教师鉴权失败"))
+        case Some(id) =>
+          IO(logger.info(s"[Step 1.2] 教师 token 验证成功，teacherID 是: ${id}")) >>
+            IO.pure(id)
+      }
 
-      _ <- IO(logger.info(s"查询到课程组列表，数量为: ${courseGroups.size}"))
+      // Step 2: Query CourseGroupTable to retrieve all course groups created by teacherID
+      _ <- IO(logger.info(s"[Step 2] 开始查询由教师创建的课程组，teacherID: ${teacherID}"))
+      courseGroups <- queryOwnCourseGroups(teacherID)
+
+      _ <- IO(logger.info(s"[Step 3] 查询完成，共找到 ${courseGroups.length} 个课程组"))
     } yield courseGroups
   }
 
-  /**
-   * 验证教师 token 并获取教师 ID
-   * @param teacherToken 教师 token
-   * @return 教师 ID
-   */
-  private def validateTokenAndFetchTeacherID(teacherToken: String)(using PlanContext): IO[Int] = {
-    for {
-      _ <- IO(logger.info(s"开始验证教师 token: ${teacherToken}"))
-      teacherIDOpt <- validateTeacherToken(teacherToken)
-
-      // 鉴权失败处理
-      _ <- teacherIDOpt match {
-        case None => IO(logger.error(s"教师 token 验证失败: ${teacherToken}"))
-        case Some(_) => IO.unit
-      }
-
-      teacherID <- teacherIDOpt match {
-        case None => IO.raiseError(new IllegalStateException("教师鉴权失败。"))
-        case Some(id) => IO.pure(id)
-      }
-    } yield teacherID
-  }
-
-  /**
-   * 查询课程组信息
-   * @param teacherID 教师 ID
-   * @return List[CourseGroup]
-   */
-  private def queryCourseGroupsByTeacherID(teacherID: Int)(using PlanContext): IO[List[CourseGroup]] = {
+  private def queryOwnCourseGroups(teacherID: Int)(using PlanContext): IO[List[CourseGroup]] = {
     val sql =
       s"""
          |SELECT course_group_id, name, credit, owner_teacher_id
          |FROM ${schemaName}.course_group_table
          |WHERE owner_teacher_id = ?;
-      """.stripMargin
+       """.stripMargin
 
     for {
-      _ <- IO(logger.info(s"执行查询课程组信息的 SQL 指令: ${sql}"))
-      rowsJson <- readDBRows(
-        sql,
-        List(SqlParameter("Int", teacherID.toString))
-      )
-      courseGroups <- IO(rowsJson.map(decodeType[CourseGroup]))
+      _ <- IO(logger.info(s"[queryOwnCourseGroups] 执行数据库查询指令: $sql"))
+      rows <- readDBRows(sql, List(SqlParameter("Int", teacherID.toString)))
+
+      courseGroups <- IO {
+        rows.map { row =>
+          CourseGroup(
+            courseGroupID = decodeField[Int](row, "course_group_id"),
+            name = decodeField[String](row, "name"),
+            credit = decodeField[Int](row, "credit"),
+            ownerTeacherID = decodeField[Int](row, "owner_teacher_id"),
+            authorizedTeachers = List.empty // Assuming this is not retrieved from CourseGroupTable
+          )
+        }
+      }
+
+      _ <- IO(logger.info(s"[queryOwnCourseGroups] 完成从数据库转换为 CourseGroup 对象，共转换 ${courseGroups.length} 个课程组"))
     } yield courseGroups
   }
 }
